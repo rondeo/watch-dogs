29225855f2cb9ed4890620d828e13dbe86d6b140
vladt <vladstitov@gmail.com>
2018-02-03 13:50:12 -0500

    ETH

---
+++ src/app/eth/eth-main/eth-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eth-main',
+  templateUrl: './eth-main.component.html',
+  styleUrls: ['./eth-main.component.css']
+})
+export class EthMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
